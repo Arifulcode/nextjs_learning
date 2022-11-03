268fbff600bc64a39bb23c5ff643138aee02fc7f
Ariful Islam Polash <arifulislampolash.25.fswad@gmail.com>
2022-11-04 00:08:24 +0600

    link component navigation

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,9 +1,36 @@
-function ProductList(){
+import Link from 'next/link'
+function ProductList({productId=100 }){
     return (
         <>
-            <h3>product 1</h3>
+        <h2>
+           <Link href='/'>
+                home
+            </Link> 
+        </h2>
+        <h2>
+           <Link href='/product/1'>
+                Product 1
+            </Link> 
+        </h2>
+        <h2>
+           <Link href='/product/2'>
+                Product 2
+            </Link> 
+        </h2>
+        <h2>
+           <Link href='/product/3'>
+                Product 3
+            </Link> 
+        </h2>
+        <h2>
+           <Link href={`/product/${productId}`}>
+                <h1>Product {productId}</h1>
+            </Link> 
+        </h2>
+        
+            {/* <h3>product 1</h3>
             <h3>product 2</h3>
-            <h3>product 3</h3>
+            <h3>product 3</h3> */}
         </>
     )
 }
